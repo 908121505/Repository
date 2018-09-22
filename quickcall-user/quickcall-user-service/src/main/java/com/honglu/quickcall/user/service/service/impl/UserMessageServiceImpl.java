845ef4c7dadf48675e7304255f95a7ad5325135d
duanjun <D7prSrWpY2>
2018-09-22 18:13:43 +0800

    用户消息服务

---
+++ quickcall-user/quickcall-user-service/src/main/java/com/honglu/quickcall/user/service/service/impl/UserMessageServiceImpl.java
@@ -0,0 +1,20 @@
+package com.honglu.quickcall.user.service.service.impl;
+
+import com.honglu.quickcall.common.api.exchange.CommonResponse;
+import com.honglu.quickcall.user.facade.exchange.request.UserUnreadMessageNumRequest;
+import com.honglu.quickcall.user.service.service.UserMessageService;
+
+/**
+ * 用户消息服务实现类
+ *
+ * @author duanjun
+ * @date 2018-09-22 17:28
+ */
+public class UserMessageServiceImpl implements UserMessageService {
+
+    @Override
+    public CommonResponse queryUserUnreadMessageNum(UserUnreadMessageNumRequest params) {
+
+        return null;
+    }
+}
