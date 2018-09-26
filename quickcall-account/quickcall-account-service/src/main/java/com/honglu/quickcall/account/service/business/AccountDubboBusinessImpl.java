5e23e44240d4a60cc18e35331af7df3081cc4191
liyingtang <liyingtang@xiaoniu.com>
2018-09-26 20:31:02 +0800

    账户

--- quickcall-account/quickcall-account-service/src/main/java/com/honglu/quickcall/account/service/business/AccountDubboBusinessImpl.java
+++ quickcall-account/quickcall-account-service/src/main/java/com/honglu/quickcall/account/service/business/AccountDubboBusinessImpl.java
@@ -45,132 +45,131 @@ import com.honglu.quickcall.user.facade.code.UserBizReturnCode;
  */
 @Service("Account.AccountDubboBusiness")
 public class AccountDubboBusinessImpl implements AccountDubboBusiness {
-    private static final Logger logger = LoggerFactory.getLogger(AccountDubboBusinessImpl.class);
-    
-    @Autowired
-    private UserAccountService userAccountService;
-    @Autowired
-    private AliPayService aliPayService;
-    @Autowired
-    private ISkillService  skillService;
-    @Autowired
-    private IOrderService  orderService;
+	private static final Logger logger = LoggerFactory.getLogger(AccountDubboBusinessImpl.class);
 
-    @Override
-    public CommonResponse excute(AbstractRequest request) {
-        logger.info("请求参数为:{}",request);
-        if (request == null) {
-            throw new BizException(BizCode.ParamError, BizCode.ParamError.desc());
-        }
+	@Autowired
+	private UserAccountService userAccountService;
+	@Autowired
+	private AliPayService aliPayService;
+	@Autowired
+	private ISkillService skillService;
+	@Autowired
+	private IOrderService orderService;
 
-        CommonResponse response = new CommonResponse();
-        try {
-            switch (request.getBizCode()) {
-            /////////////////////////////////////////////////////////////////
-            /**大V接单设置展示*/
-            case OrderRequestType.QUERY_SKILL_INFO:
-				response = skillService.querySkillInfoPersonal((SkillInfoRequest)request);
-				break;  
-			/**大V接单设置（开启关闭修改）*/
-            case OrderRequestType.UPDATE_SKILL_INFO:
-                response=  skillService.updateSkillInfoPersonal((SkillUpdateRequest)request);
-                break;
-                
-            /**首页大V技能展示*/
-            case OrderRequestType.QUERY_ORDER_FOR_FIRST_PAGE:
-            	response=  skillService.getFirstPageDaVinfo((FirstPageDaVinfoRequest)request);
-            	break;
-            /**首页技能种类展示*/
-            case OrderRequestType.QUERY_SKILL_NAME_FOR_FIRST_PAGE:
-            	response=  skillService.getFirstPageSkillinfo((FirstPageSkillinfoRequest)request);
-            	break;
-            /////////////////////////////////////////////////////////////////
-            /**获取主播开启产品*/
-            case OrderRequestType.ORDER_DAV_PRODUCT_LIST:
-            	response=  orderService.queryDaVProduct((OrderDaVProductRequest)request);
-            	break;
-            /**用户下单*/
-            case OrderRequestType.ORDER_SAVE:
-            	response=  orderService.saveOrder((OrderSaveRequest)request);
-            	break;
-            /**收到的订单*/
-            case OrderRequestType.ORDER_RECEIVE_ORDER_LIST:
-            	response=  orderService.queryReceiveOrderList((OrderReceiveOrderListRequest)request);
-            	break;
-            /**发起的订单*/
-            case OrderRequestType.ORDER_SEND_ORDER_LIST:
-            	response=  orderService.querySendOrderList((OrderSendOrderListRequest)request);
-            	break;
-            	
-           /////////////////////////////////////////////////////////////////
-            /**订单详情*/
-            case OrderRequestType.DETAIL_ORDER:
-            	response=  orderService.detailOrder((DetailOrderRequest)request);
-            	break;
-            	/**发起的订单页--去支付*/
-            case OrderRequestType.CUST_PAY_ORDER:
-            	response=  orderService.payOrder((PayOrderRequest)request);
-            	break;
-            /**发起的订单页--申请退款/完成*/
-            case OrderRequestType.CUST_APPLAY_REFUND:
-            	response=  orderService.applayRefund((ApplayRefundRequest)request);
-            	break;
-            /**发起的订单页--同意/拒绝*/
-            case OrderRequestType.CUST_CONFIRM_ORDER:
-            	response=  orderService.confirmOrder((ConfirmOrderRequest)request);
-            	break;
-            /////////////////////////////////////////////////////////////////
-            /**收到的订单页--大V接受/拒绝*/
-            case OrderRequestType.DV_RECEIVE_ORDER:
-            	response=  orderService.dvReceiveOrder((DvReceiveOrderRequest)request);
-            	break;
-            /**收到的订单页--大V立即服务*/
-            case OrderRequestType.DV_START_SERVICE:
-            	response=  orderService.dvStartService((DvStartServiceRequest)request);
-            	break;
-            /**收到的订单页--大V同意退款/拒绝*/
-            case OrderRequestType.DV_CONFRIM_ORDER:
-            	response=  orderService.dvConfirmRefund((DvConfirmRefundRequest)request);
-            	break;
-            ///////////////////////////
-            
-            
-            
-            case AccountFunctionType.CreateUserAccount:
-                //创建账户
-                response = userAccountService.createAccount((CreateUserAccountRequest) request);
-                break;
-            case AccountFunctionType.AlipayRecharge:
-            	//支付宝充值
-            	response = aliPayService.recharge((RechargeRequest)request);
-                break;
-            case AccountFunctionType.AlipayWhithdraw:
-            	//支付宝提现
-            	response = aliPayService.whthdraw((WhthdrawRequest)request);
-            	break;
-            case AccountFunctionType.BindAliaccount:
-            	//绑定支付宝
-            	response = aliPayService.bindAliaccount((BindAliaccountRequest)request);
-            	break;
-            case AccountFunctionType.AlipayNotify:
-            	response = aliPayService.alipayNotify((AlipayNotifyRequest)request);
-            	break;
-            case AccountFunctionType.QueryAccount:
-            	response = userAccountService.queryAccount((QueryAccountRequest)request);
-                default:
+	@Override
+	public CommonResponse excute(AbstractRequest request) {
+		logger.info("请求参数为:{}", request);
+		if (request == null) {
+			throw new BizException(BizCode.ParamError, BizCode.ParamError.desc());
+		}
 
-            }
-        }catch (BaseException e){
-            logger.error("接口编码为："+request.getBizCode()+"异常："+e.getMessage(), e);
-            response.setCode(e.getCode());
-            response.setMessage(e.getMessage());
-        }catch (Exception e){
-            logger.error("接口编码为："+request.getBizCode()+"异常："+e.getMessage(), e);
-            response.setCode(UserBizReturnCode.Unknown);
-            response.setMessage(e.getMessage()==null?e+"" :e.getMessage()+e);
-        }
-        logger.info("返回结果{}", response);
-        return response;
+		CommonResponse response = new CommonResponse();
+		try {
+			switch (request.getBizCode()) {
+			/////////////////////////////////////////////////////////////////
+			/** 大V接单设置展示 */
+			case OrderRequestType.QUERY_SKILL_INFO:
+				response = skillService.querySkillInfoPersonal((SkillInfoRequest) request);
+				break;
+			/** 大V接单设置（开启关闭修改） */
+			case OrderRequestType.UPDATE_SKILL_INFO:
+				response = skillService.updateSkillInfoPersonal((SkillUpdateRequest) request);
+				break;
 
-    }
+			/** 首页大V技能展示 */
+			case OrderRequestType.QUERY_ORDER_FOR_FIRST_PAGE:
+				response = skillService.getFirstPageDaVinfo((FirstPageDaVinfoRequest) request);
+				break;
+			/** 首页技能种类展示 */
+			case OrderRequestType.QUERY_SKILL_NAME_FOR_FIRST_PAGE:
+				response = skillService.getFirstPageSkillinfo((FirstPageSkillinfoRequest) request);
+				break;
+			/////////////////////////////////////////////////////////////////
+			/** 获取主播开启产品 */
+			case OrderRequestType.ORDER_DAV_PRODUCT_LIST:
+				response = orderService.queryDaVProduct((OrderDaVProductRequest) request);
+				break;
+			/** 用户下单 */
+			case OrderRequestType.ORDER_SAVE:
+				response = orderService.saveOrder((OrderSaveRequest) request);
+				break;
+			/** 收到的订单 */
+			case OrderRequestType.ORDER_RECEIVE_ORDER_LIST:
+				response = orderService.queryReceiveOrderList((OrderReceiveOrderListRequest) request);
+				break;
+			/** 发起的订单 */
+			case OrderRequestType.ORDER_SEND_ORDER_LIST:
+				response = orderService.querySendOrderList((OrderSendOrderListRequest) request);
+				break;
+
+			/////////////////////////////////////////////////////////////////
+			/** 订单详情 */
+			case OrderRequestType.DETAIL_ORDER:
+				response = orderService.detailOrder((DetailOrderRequest) request);
+				break;
+			/** 发起的订单页--去支付 */
+			case OrderRequestType.CUST_PAY_ORDER:
+				response = orderService.payOrder((PayOrderRequest) request);
+				break;
+			/** 发起的订单页--申请退款/完成 */
+			case OrderRequestType.CUST_APPLAY_REFUND:
+				response = orderService.applayRefund((ApplayRefundRequest) request);
+				break;
+			/** 发起的订单页--同意/拒绝 */
+			case OrderRequestType.CUST_CONFIRM_ORDER:
+				response = orderService.confirmOrder((ConfirmOrderRequest) request);
+				break;
+			/////////////////////////////////////////////////////////////////
+			/** 收到的订单页--大V接受/拒绝 */
+			case OrderRequestType.DV_RECEIVE_ORDER:
+				response = orderService.dvReceiveOrder((DvReceiveOrderRequest) request);
+				break;
+			/** 收到的订单页--大V立即服务 */
+			case OrderRequestType.DV_START_SERVICE:
+				response = orderService.dvStartService((DvStartServiceRequest) request);
+				break;
+			/** 收到的订单页--大V同意退款/拒绝 */
+			case OrderRequestType.DV_CONFRIM_ORDER:
+				response = orderService.dvConfirmRefund((DvConfirmRefundRequest) request);
+				break;
+			///////////////////////////
+
+			case AccountFunctionType.CreateUserAccount:
+				// 创建账户
+				response = userAccountService.createAccount((CreateUserAccountRequest) request);
+				break;
+			case AccountFunctionType.AlipayRecharge:
+				// 支付宝充值
+				response = aliPayService.recharge((RechargeRequest) request);
+				break;
+			case AccountFunctionType.AlipayWhithdraw:
+				// 支付宝提现
+				response = aliPayService.whthdraw((WhthdrawRequest) request);
+				break;
+			case AccountFunctionType.BindAliaccount:
+				// 绑定支付宝
+				response = aliPayService.bindAliaccount((BindAliaccountRequest) request);
+				break;
+			case AccountFunctionType.AlipayNotify:
+				response = aliPayService.alipayNotify((AlipayNotifyRequest) request);
+				break;
+			case AccountFunctionType.QueryAccount:
+				response = userAccountService.queryAccount((QueryAccountRequest) request);
+				break;
+			default:
+
+			}
+		} catch (BaseException e) {
+			logger.error("接口编码为：" + request.getBizCode() + "异常：" + e.getMessage(), e);
+			response.setCode(e.getCode());
+			response.setMessage(e.getMessage());
+		} catch (Exception e) {
+			logger.error("接口编码为：" + request.getBizCode() + "异常：" + e.getMessage(), e);
+			response.setCode(UserBizReturnCode.Unknown);
+			response.setMessage(e.getMessage() == null ? e + "" : e.getMessage() + e);
+		}
+		logger.info("返回结果{}", response);
+		return response;
+
+	}
 }
