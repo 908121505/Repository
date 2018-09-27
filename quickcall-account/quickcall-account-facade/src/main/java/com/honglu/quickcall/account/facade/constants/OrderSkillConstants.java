7a86d67f0c8d6506845d5d474372e1308164b83f
chenliuguang <chenliuguang@chenliuguang.xiaoniu.com>
2018-09-27 10:44:13 +0800

    修改订单状态定义

--- quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/constants/OrderSkillConstants.java
+++ quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/constants/OrderSkillConstants.java
@@ -52,40 +52,40 @@ public interface OrderSkillConstants {
 	
 	/**订单状态1.下单未支付（已下单，未支付）; */
 	public static final Integer  ORDER_STATUS_NOT_PAY = 1 ;
-	/**订单状态2.下单未支付系统取消（已下单，30分钟内未支付）*/
-	public static final Integer  ORDER_STATUS_CANCEL_NOT_PAY = 2 ;//
-	/**订单状态3.已支付（已下单，支付完成）*/
-	public static final Integer  ORDER_STATUS_PAYED = 3 ;
-	/**订单状态4.超时未接（已支付，大V30分钟内未接单）*/
-	public static final Integer  ORDER_STATUS_CANCEL_PAYED_DV_RECEIVE_OVER_TIME = 4 ;//
-	/**订单状态5.大V接单前用户自主取消;*/
-	public static final Integer  ORDER_STATUS_CANCEL_PAYED_USER_SELE_CANCEL = 5 ;
-	/**订单状态6.大V拒绝订单（支付完成，大V拒绝，客户退款）;*/
-	public static final Integer  ORDER_STATUS_PAYED_DV_REFUSE = 6 ;
-	/**订单状态7.大V接受订单;*/
-	public static final Integer  ORDER_STATUS_PAYED_DV_ACCEPT_ORDER = 7 ;
-	/**订单状态8.大V接单后用户取消订单（用户退款）;*/
-	public static final Integer  ORDER_STATUS_CANCLE_DV_ACCEPT_USER_SELF_CANCLE = 8 ;
-	/**订单状态9.大V确认开始订单;*/
-	public static final Integer  ORDER_STATUS_PAYED_DV_CONFIRM_START = 9 ;
-	/**订单状态10.用户同意（响应大V立即服务请求）;*/
-	public static final Integer  ORDER_STATUS_CUST_AGREE_DV_START_SERVICE = 10 ;
-	/**订单状态11.用户拒绝（响应大V立即服务请求）;*/
-	public static final Integer  ORDER_STATUS_CUST_REFUSE_DV_START_SERVICE = 11 ;
-	/**订单状态12.进行中（用户同意或者到约定的订单开始时间）;*/
-	public static final Integer  ORDER_STATUS_GOINGING = 12 ;
-	/**订单状态13.用户申请退款;*/
-	public static final Integer  ORDER_STATUS_USER_APPLAY_REFUND = 13 ;
-	/**订单状态14.订单作废（大V同意退款，客户退款）;*/
-	public static final Integer  ORDER_STATUS_CANCEL_DV_AGREE_REFUND = 14 ;
-	/**订单状态15.订单完成（大V拒绝退款）;*/
-	public static final Integer  ORDER_STATUS_END_DV_REFUSE = 15 ;
-	/**订单状态16.订单完成（正常完成）;*/
-	public static final Integer  ORDER_STATUS_END = 16 ;
-	/**订单状态17.支付之前取消;*/
-	public static final Integer  ORDER_STATUS_CANCEL_BEFORE_PAY = 17 ;
-	/**订单状态18.进行中之前取消;*/
-	public static final Integer  ORDER_STATUS_CANCEL_BEFORE_ING = 18 ;
+	/**订单状态2.支付之前取消*/
+	public static final Integer  ORDER_STATUS_CANCEL_BEFORE_PAY = 2 ;//
+	/**订单状态3.下单未支付系统取消（已下单，30分钟内未支付）*/
+	public static final Integer  ORDER_STATUS_CANCEL_NOT_PAY = 3 ;//
+	/**订单状态4.已支付（已下单，支付完成）*/
+	public static final Integer  ORDER_STATUS_PAYED = 4 ;
+	/**订单状态5.超时未接（已支付，大V30分钟内未接单）*/
+	public static final Integer  ORDER_STATUS_CANCEL_PAYED_DV_RECEIVE_OVER_TIME = 5 ;//
+	/**订单状态6.大V接单前用户自主取消;*/
+	public static final Integer  ORDER_STATUS_CANCEL_PAYED_USER_SELE_CANCEL = 6 ;
+	/**订单状态7.大V拒绝订单（支付完成，大V拒绝，客户退款）;*/
+	public static final Integer  ORDER_STATUS_PAYED_DV_REFUSE = 7 ;
+	/**订单状态8.大V接受订单;*/
+	public static final Integer  ORDER_STATUS_PAYED_DV_ACCEPT_ORDER = 8 ;
+	/**订单状态9.大V接单后用户取消订单（用户退款）;*/
+	public static final Integer  ORDER_STATUS_CANCLE_DV_ACCEPT_USER_SELF_CANCLE = 9 ;
+	/**订单状态10.大V确认开始订单;*/
+	public static final Integer  ORDER_STATUS_PAYED_DV_CONFIRM_START = 10 ;
+	/**订单状态11.用户同意（响应大V立即服务请求）;*/
+	public static final Integer  ORDER_STATUS_CUST_AGREE_DV_START_SERVICE = 11 ;
+	/**订单状态12.订单进行之前取消（客户退款）;*/
+	public static final Integer  ORDER_STATUS_CANCEL_BEFORE_ING = 12 ;
+	/**订单状态13.用户拒绝（响应大V立即服务请求）;*/
+	public static final Integer  ORDER_STATUS_CUST_REFUSE_DV_START_SERVICE = 13 ;
+	/**订单状态14.进行中（用户同意或者到约定的订单开始时间）;*/
+	public static final Integer  ORDER_STATUS_GOINGING = 14 ;
+	/**订单状态15.用户申请退款;*/
+	public static final Integer  ORDER_STATUS_USER_APPLAY_REFUND = 15 ;
+	/**订单状态16.订单作废（大V同意退款，客户退款）;*/
+	public static final Integer  ORDER_STATUS_CANCEL_DV_AGREE_REFUND = 16 ;
+	/**订单状态17.订单完成（大V拒绝退款）;*/
+	public static final Integer  ORDER_STATUS_END_DV_REFUSE = 17 ;
+	/**订单状态18.订单完成（正常完成）;*/
+	public static final Integer  ORDER_STATUS_END = 18 ;
 	
 	
 }
