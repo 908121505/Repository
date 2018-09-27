83c9b8a87481a40734f5e9d1c7c5596546feac5d
chenliuguang <chenliuguang@chenliuguang.xiaoniu.com>
2018-09-27 14:53:17 +0800

    修改常量定义

--- quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/code/AccountBizReturnCode.java
+++ quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/code/AccountBizReturnCode.java
@@ -19,10 +19,11 @@ public class AccountBizReturnCode extends BizCode {
     public static final BizCode JdbcError = new AccountBizReturnCode("JdbcError","996",MyServiceCode.ACCOUNT,"操作数据库异常");
     public static final BizCode CheckSignError = new AccountBizReturnCode("CheckSignError","997", MyServiceCode.ACCOUNT,"请求参数验签不正确");
     public static final BizCode SystemParamError = new AccountBizReturnCode("SystemParamError","998", MyServiceCode.ACCOUNT,"系统参数初始化异常");
-    public static final BizCode Unknown = new AccountBizReturnCode("Unknown","999", MyServiceCode.ACCOUNT,"未知异常");
+//    public static final BizCode Unknown = new AccountBizReturnCode("Unknown","999", MyServiceCode.ACCOUNT,"未知异常");
     
     //订单中心使用异常号段：700-799-------开始
     
+    public static final BizCode order_ = new AccountBizReturnCode("Unknown","999", MyServiceCode.ACCOUNT,"未知异常");
     
     
     //订单中心使用异常号段：700-799-------结束
