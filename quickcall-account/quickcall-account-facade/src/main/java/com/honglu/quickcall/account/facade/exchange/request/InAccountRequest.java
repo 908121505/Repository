29a065fd193ecd2720d3bcd9124cfc53358d241a
liyingtang <liyingtang@xiaoniu.com>
2018-10-15 16:11:45 +0800

    账户 出入账流水

--- quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/exchange/request/InAccountRequest.java
+++ quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/exchange/request/InAccountRequest.java
@@ -6,20 +6,34 @@ import com.honglu.quickcall.account.facade.code.AccountFunctionType;
 import com.honglu.quickcall.common.api.exchange.AbstractRequest;
 
 public class InAccountRequest extends AbstractRequest {
-      private Long userId;
-      private BigDecimal amount;
+	private Long userId;
+	private BigDecimal amount;
+	private Integer transferType;// 1 +可用金额 2+提现金额
+
+	public Integer getTransferType() {
+		return transferType;
+	}
+
+	public void setTransferType(Integer transferType) {
+		this.transferType = transferType;
+	}
+
 	public Long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
+
 	public BigDecimal getAmount() {
 		return amount;
 	}
+
 	public void setAmount(BigDecimal amount) {
 		this.amount = amount;
 	}
+
 	@Override
 	public String getBizCode() {
 		// TODO Auto-generated method stub
---
+++ quickcall-account/quickcall-account-service/src/main/java/com/honglu/quickcall/account/service/service/AccountService.java
@@ -0,0 +1,39 @@
+package com.honglu.quickcall.account.service.service;
+
+import java.math.BigDecimal;
+
+import com.honglu.quickcall.account.facade.entity.Account;
+import com.honglu.quickcall.account.facade.enums.AccountBusinessTypeEnum;
+import com.honglu.quickcall.account.facade.enums.TransferTypeEnum;
+
+public interface AccountService {
+	/**
+	 * 查询账户
+	 * 
+	 * @param request
+	 * @return
+	 */
+	Account queryAccount(Long customerId);
+
+	/**
+	 * 入账
+	 * 
+	 * @param customerId
+	 * @param amount
+	 * @param transferType
+	 * @param accountBusinessType
+	 */
+	void inAccount(Long customerId, BigDecimal amount, TransferTypeEnum transferType,
+			AccountBusinessTypeEnum accountBusinessType);
+
+	/**
+	 * 入账
+	 * 
+	 * @param customerId
+	 * @param amount
+	 * @param transferType
+	 * @param accountBusinessType
+	 */
+	void outAccount(Long customerId, BigDecimal amount, TransferTypeEnum transferType,
+			AccountBusinessTypeEnum accountBusinessType);
+}
