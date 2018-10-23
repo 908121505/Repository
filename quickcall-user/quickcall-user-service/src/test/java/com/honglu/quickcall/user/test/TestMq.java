b436213f3e1dd23395bfed59fbe8baaebb9552af
duanjun <D7prSrWpY2>
2018-10-23 11:10:04 +0800

    user配置MQ

---
+++ quickcall-user/quickcall-user-service/src/test/java/com/honglu/quickcall/user/test/TestMq.java
@@ -0,0 +1,28 @@
+package com.honglu.quickcall.user.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * MQ测试
+ *
+ * @author duanjun
+ * @date 2018-10-23 10:57
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class TestMq {
+
+    @Resource
+    private AmqpTemplate amqpTemplate;
+
+    @Test
+    public void testSend(){
+        amqpTemplate.convertAndSend("userCenter-mq-exchange", "queue_userCenter_for_experience_key", "测试消息");
+    }
+}
