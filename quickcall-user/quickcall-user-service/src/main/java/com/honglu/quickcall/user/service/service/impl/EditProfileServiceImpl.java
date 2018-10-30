b09ee1580f809e9a234d9db65a789897e1a718b5
chenpeng <chenpeng@xiaoniu.com>
2018-10-30 20:02:35 +0800

    修改头像，直接保存到用户表

--- quickcall-user/quickcall-user-service/src/main/java/com/honglu/quickcall/user/service/service/impl/EditProfileServiceImpl.java
+++ quickcall-user/quickcall-user-service/src/main/java/com/honglu/quickcall/user/service/service/impl/EditProfileServiceImpl.java
@@ -269,10 +269,8 @@ public class EditProfileServiceImpl implements EditProfileService {
 				.selectByCustomerIdAndType(params.getCustomerId(), AppearanceTypeEnum.HEAD_PORTRAIT.getCode());
 		if (null != customerAppearance) {
 
-			*//*
-			 * customerAppearance.setAuditAppearance(params.getHeadPortraitUrl());
-			 * customerAppearance.setAuditStatus(0);
-			 *//*
+//			 customerAppearance.setAuditAppearance(params.getHeadPortraitUrl());
+//			 customerAppearance.setAuditStatus(0);
 
 			// 修改头像这一版不用审核 陈鹏 2018-10-24
 			customerAppearance.setAppearance(params.getHeadPortraitUrl());
