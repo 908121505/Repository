3b548924becd217f5ed3911f76ed19085fa37a6e
zhaozheyi <zhaozheyi@xiaoniu.com>
2018-11-01 22:20:15 +0800

    还原获取融云重试的方法

--- quickcall-common/quickcall-cmomon-third/src/main/java/com/honglu/quickcall/common/third/rongyun/util/RongYunUtil.java
+++ quickcall-common/quickcall-cmomon-third/src/main/java/com/honglu/quickcall/common/third/rongyun/util/RongYunUtil.java
@@ -65,11 +65,6 @@ public class RongYunUtil {
 		try {
 			userRefreshResult = rongCloud.user.refresh(id, username, imageUrl);
 		} catch (Exception e) {
-			try {
-				userRefreshResult = rongCloud.user.refresh(id, username, imageUrl);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
 			e.printStackTrace();
 		}
 		return userRefreshResult;
