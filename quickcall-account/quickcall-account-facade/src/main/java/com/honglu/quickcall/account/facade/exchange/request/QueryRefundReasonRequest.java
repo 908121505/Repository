6b8835d4d82af04e46d2e83732be1b4324a5d0f8
chenpeng <chenpeng@xiaoniu.com>
2018-11-02 20:02:22 +0800

    QueryRefundReasonRequest.java

--- quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/exchange/request/QueryRefundReasonRequest.java
+++ quickcall-account/quickcall-account-facade/src/main/java/com/honglu/quickcall/account/facade/exchange/request/QueryRefundReasonRequest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //package com.honglu.quickcall.account.facade.exchange.request;
 //
 //import com.honglu.quickcall.account.facade.code.OrderRequestType;
@@ -26,32 +25,4 @@
 //
 //
 //}
-=======
-package com.honglu.quickcall.account.facade.exchange.request;
 
-import com.honglu.quickcall.account.facade.code.OrderRequestType;
-import com.honglu.quickcall.common.api.exchange.AbstractRequest;
-
-/**   
- * Copyright © 2018 www.xiaoniu.com All rights reserved.
- * 
- * 功能描述：查询退款理由
- * @Package: com.honglu.quickcall.account.facade.exchange.req
- * @author: chenliuguang   
- * @date: 2018年9月28日 下午4:59:48 
- */
-public class QueryRefundReasonRequest extends AbstractRequest {
-    
-
-	private static final long serialVersionUID = -7468782505287045144L;
-
-	@Override
-	public String getBizCode() {
-		return OrderRequestType.QUERY_REFUND_REASON;
-	}
-
-
-
-
-}
->>>>>>> refs/remotes/origin/develop-databury
