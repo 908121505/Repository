f2c82fabaf636eeb486bdc6a82bfbde2dd2dff43
xiangping <xiangping@xiaoniu.com>
2018-11-02 16:07:06 +0800

    项目明更改

---
+++ quickcall-producer/quickcall-producer-facade/src/main/java/com/honglu/quickcall/producer/facade/req/databury/DataBuriedPointLoginReq.java
@@ -0,0 +1,58 @@
+package com.honglu.quickcall.producer.facade.req.databury;
+
+import com.honglu.quickcall.producer.facade.req.abs.AbstractBaseReq;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * 登陆成功--数据埋点--数据
+ *
+ * @author xiangping
+ * @date 2018-10-31 11:02
+ */
+public class DataBuriedPointLoginReq extends AbstractBaseReq {
+
+    /**登陆方式**/
+    @NotBlank
+    private String loginmethod;
+
+    /**用户id**/
+    @NotBlank
+    private String user_id;
+
+    /**电话号码**/
+    @NotBlank
+    private String phoneNumber;
+
+    public String getLoginmethod() {
+        return loginmethod;
+    }
+
+    public void setLoginmethod(String loginmethod) {
+        this.loginmethod = loginmethod;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "DataBuriedPointLoginResp{" +
+                "loginmethod='" + loginmethod + '\'' +
+                ", user_id='" + user_id + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
---
+++ quickcall-producer/quickcall-producer-facade/src/main/java/com/honglu/quickcall/producer/facade/resp/databury/DataBuriedPointRegistResp.java
@@ -0,0 +1,13 @@
+package com.honglu.quickcall.producer.facade.resp.databury;
+
+import com.honglu.quickcall.producer.facade.resp.abs.AbstractBaseResp;
+
+/**
+ * 注册成功--数据埋点--数据
+ *
+ * @author xiangping
+ * @date 2018-10-31 11:02
+ */
+public class DataBuriedPointRegistResp extends AbstractBaseResp {
+
+}
