b569b299204a9a5817b716e6d0488dd3ef9243b0
duanjun <D7prSrWpY2>
2018-11-05 22:10:27 +0800

    初始化大V排名数据

--- quickcall-user/quickcall-user-service/src/main/java/com/honglu/quickcall/user/service/service/impl/ScoreRankServiceImpl.java
+++ quickcall-user/quickcall-user-service/src/main/java/com/honglu/quickcall/user/service/service/impl/ScoreRankServiceImpl.java
@@ -189,18 +189,18 @@ public class ScoreRankServiceImpl implements ScoreRankService {
         LOGGER.info("初始化声优声量评分排名数据 ----------- start");
         // 删除不是大V列表的排名数据
         int deleteNum = bigvScoreMapper.deleteNotBigvData();
-        LOGGER.info("从【bigv_score】表删除不是大V的数量条数：{}" + deleteNum);
+        LOGGER.info("从【bigv_score】表删除不是大V的数量条数：" + deleteNum);
 
         // 删除不在大V技能列表的排名数据
         deleteNum = bigvSkillScoreMapper.deleteNotBigvData();
-        LOGGER.info("从【bigv_skill_score】表删除不是大V技能表中的数据条数：{}" + deleteNum);
+        LOGGER.info("从【bigv_skill_score】表删除不是大V技能表中的数据条数：" + deleteNum);
 
         // 查询不在大V总排名表中的大VID
         List<Long> customerIds = bigvScoreMapper.selectNeedInsertBigvData();
+        LOGGER.info("插入【bigv_score】表差的数据条数：" + customerIds.size());
         if (!customerIds.isEmpty()) {
-            LOGGER.info("插入【bigv_score】表差的数据条数：{}" + customerIds.size());
             BigvScore bigScore;
-            for(Long customerId: customerIds){
+            for (Long customerId : customerIds) {
                 bigScore = new BigvScore();
                 bigScore.setId(UUIDUtils.getId());
                 bigScore.setCustomerId(customerId);
@@ -212,10 +212,10 @@ public class ScoreRankServiceImpl implements ScoreRankService {
 
         // 查询不在大V技能排名表的大V技能数据
         List<BigvSkillScore> bigvSkillScores = bigvSkillScoreMapper.selectNeedInsertBigvData();
-        if(!bigvSkillScores.isEmpty()){
-            LOGGER.info("插入【bigv_skill_score】表差的数据条数：{}" + customerIds.size());
+        LOGGER.info("插入【bigv_skill_score】表差的数据条数：" + customerIds.size());
+        if (!bigvSkillScores.isEmpty()) {
             BigvSkillScore bigvSkillScore;
-            for(BigvSkillScore skillScore: bigvSkillScores){
+            for (BigvSkillScore skillScore : bigvSkillScores) {
                 bigvSkillScore = new BigvSkillScore();
                 bigvSkillScore.setId(UUIDUtils.getId());
                 bigvSkillScore.setCustomerId(skillScore.getCustomerId());
@@ -241,6 +241,7 @@ public class ScoreRankServiceImpl implements ScoreRankService {
             // 判断当前客户是否为大V
             if (customerMapper.judgeCustomerIsBigv(bigvSkillScore.getCustomerId()) == 0) {
                 // 若不是大V -- 清楚技能排名表数据
+                LOGGER.info("从【bigv_score 和 bigv_skill_score】删除不是大V的数据：" + bigvSkillScore.getCustomerId());
                 bigvSkillScoreMapper.deleteDataByCustomerId(bigvSkillScore.getCustomerId());
                 bigvScoreMapper.deleteDataByCustomerId(bigvSkillScore.getCustomerId());
             }
@@ -252,10 +253,12 @@ public class ScoreRankServiceImpl implements ScoreRankService {
             // 循环计算声优该技能的累积得分值
             for (Order order : orderList) {
                 // 计算该订单对应的技能的评分
-                totalScore.add(calculateOrderSkillScore2(order, order.getEvaluateStart()));
+                totalScore = totalScore.add(calculateOrderSkillScore2(order));
             }
 
             // 存入技能排名表
+            LOGGER.info("更新表【bigv_skill_score】大V的技能排名："
+                    + bigvSkillScore.getCustomerSkillId() + " - totalScore：" + totalScore + " - orderTotal：" + orderList.size());
             bigvSkillScoreMapper.updateBigvSkillScore2(bigvSkillScore.getCustomerSkillId(), totalScore, orderList.size());
         }
 
@@ -266,19 +269,27 @@ public class ScoreRankServiceImpl implements ScoreRankService {
         return ResultUtils.resultSuccess();
     }
 
-    private BigDecimal calculateOrderSkillScore2(Order order, Integer evaluateStars) {
+    private BigDecimal calculateOrderSkillScore2(Order order) {
         // 订单累计数，暂时用字段 paymentType 接收
         Integer orderTotal = order.getPaymentType();
+        if (order.getServicePrice() == null) {
+            order.setServicePrice(new BigDecimal(0));
+        }
+        if (!Objects.equals(order.getCustomerIsEvaluate(), 1)) {
+            order.setEvaluateStart(3);
+        }
+        if (order.getEvaluateStart() == null) {
+            order.setEvaluateStart(0);
+        }
 
         // 计算技能总比价得分
         BigDecimal orderTotalScore = new BigDecimal((2 / Math.log10(orderTotal + 6))
                 * 10 * ScoreRankConstants.PLATFORM_ORDER_NUM_TOTAL_WEIGHT);
-
         // 计算技能笔单价得分
         BigDecimal servicePriceScore = order.getServicePrice().multiply(new BigDecimal(ScoreRankConstants.PLATFORM_SINGLE_ORDER_PRICE_WEIGHT));
 
         // 计算总得分
-        BigDecimal valueScore = orderTotalScore.add(servicePriceScore).multiply(calculateEvaluateScore(evaluateStars, order.getOrderNum()));
+        BigDecimal valueScore = orderTotalScore.add(servicePriceScore).multiply(calculateEvaluateScore(order.getEvaluateStart(), order.getOrderNum()));
 
         // 四舍五入取整
         return valueScore.setScale(0, BigDecimal.ROUND_HALF_UP);
