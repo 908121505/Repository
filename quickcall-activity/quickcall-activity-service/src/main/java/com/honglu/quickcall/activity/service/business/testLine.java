622e97c6144eac0891c892ed00116b8d150fa4e6
zhaozheyi <zhaozheyi@xiaoniu.com>
2018-11-06 19:12:52 +0800

    测试代码行数

--- quickcall-activity/quickcall-activity-service/src/main/java/com/honglu/quickcall/activity/service/business/testLine.java
+++ quickcall-activity/quickcall-activity-service/src/main/java/com/honglu/quickcall/activity/service/business/testLine.java
@@ -3,6 +3,11 @@ package com.honglu.quickcall.activity.service.business;
 public class testLine {
 	public static void aaa(){
 		System.out.println("asfds");
+		System.out.println("asfds");
+		System.out.println("asfds");
+		System.out.println("asfds");
+		System.out.println("asfds");
+		System.out.println("asfds");
 	}
 	
 	//bbb
