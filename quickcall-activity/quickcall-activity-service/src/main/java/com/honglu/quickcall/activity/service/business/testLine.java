d81960c5340ff356b58295f03c21be9ef548511d
zhaozheyi <zhaozheyi@xiaoniu.com>
2018-11-06 17:54:44 +0800

    测试git行数

---
+++ quickcall-activity/quickcall-activity-service/src/main/java/com/honglu/quickcall/activity/service/business/testLine.java
@@ -0,0 +1,12 @@
+package com.honglu.quickcall.activity.service.business;
+
+public class testLine {
+	public static void aaa(){
+		System.out.println("asfds");
+	}
+	
+	//bbb
+	public static void bbb(){
+		System.out.println("asfds");
+	}
+}
