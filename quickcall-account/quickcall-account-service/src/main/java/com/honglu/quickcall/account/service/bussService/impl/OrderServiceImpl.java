feae2a5f828d11b506d97a7581a225d92c9e416b
chenliuguang <chenliuguang@chenliuguang.xiaoniu.com>
2018-11-08 14:24:26 +0800

    修改接单设置

--- quickcall-account/quickcall-account-service/src/main/java/com/honglu/quickcall/account/service/bussService/impl/OrderServiceImpl.java
+++ quickcall-account/quickcall-account-service/src/main/java/com/honglu/quickcall/account/service/bussService/impl/OrderServiceImpl.java
@@ -362,10 +362,10 @@ public class OrderServiceImpl implements IOrderService {
 		}
 		LOGGER.info("======>>>>>queryReceiveOrderList()入参：" + request.toString());
 		Long customerId = request.getCustomerId();
-		String customerJson = JedisUtil.get(RedisKeyConstants.USER_CUSTOMER_INFO + customerId);
-		if (StringUtils.isEmpty(customerJson)) {
-			return ResultUtils.result(BizCode.CustomerNotExist);
-		}
+//		String customerJson = JedisUtil.get(RedisKeyConstants.USER_CUSTOMER_INFO + customerId);
+//		if (StringUtils.isEmpty(customerJson)) {
+//			return ResultUtils.result(BizCode.CustomerNotExist);
+//		}
 		Integer orderStatusParam = request.getOrderStatus();
 		List<Integer> statusList = commonService.getReceiveOrderStatusList(orderStatusParam);
 
