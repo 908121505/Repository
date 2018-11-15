e283d0551f7c31ee060241b44bbeaa9eb17fc240
liuxifeng <liuxifeng@liuxifeng.xiaoniu.com>
2018-11-15 11:30:16 +0800

    强制完成和强制取消 用户上传，需由“手机号”修改为轻音ID； 用户上传导入文件失败。

--- quickcall-admin/src/main/java/com/calf/module/export/controller/ImportDataFileController.java
+++ quickcall-admin/src/main/java/com/calf/module/export/controller/ImportDataFileController.java
@@ -6,6 +6,7 @@ import com.gexin.fastjson.JSON;
 import com.honglu.quickcall.common.core.util.UUIDUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.POIXMLException;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -40,21 +41,34 @@ public class ImportDataFileController {
 
     /**
      * 手机格式正则校验
+     * app_id正则
      */
     private static Pattern PHONE_PATTERN = Pattern.compile("^((1[0-9]))\\d{9}$");
+    private static Pattern APPID_PATTERN = Pattern.compile("^\\d{8}$");
 
     /**
      * 导入消息记录
+     * @throws IOException 
      */
     @RequestMapping(value = "/message.htm", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, Object> messageRecord(@RequestParam("file") MultipartFile file) throws IOException {
+    public Map<String, Object> messageRecord(@RequestParam("file") MultipartFile file) throws IOException{
         Map<String, Object> result = new HashMap<>();
-        XSSFRow row = null;
-        XSSFWorkbook wb = new XSSFWorkbook(file.getInputStream());
+        XSSFRow row = null;  
+        XSSFWorkbook wb=null;
+		try {
+			wb = new XSSFWorkbook(file.getInputStream());
+		} catch (POIXMLException e1) {
+			LOGGER.error("文件导入发生异常，文件名：{}，错误原因：{}", file.getName(), e1.getMessage());
+			result.put("message","文件格式有误，请导入.xlsx类型文件");
+			result.put("code","2000");
+			return result;
+		}
         try {
             XSSFSheet xs = wb.getSheetAt(0);
             if (xs == null) {
+            	result.put("code", 2005);
+            	result.put("message", "文件类容格式有误");
                 return result;
             }
             Set<String> checkSet =  new HashSet<String>();
@@ -69,23 +83,33 @@ public class ImportDataFileController {
             }
             for (int rowNum = 0; rowNum < rowNumTotal; rowNum++) {
                 row = xs.getRow(rowNum);
-                String phoneNum =  getValue(row.getCell(0));
-                Matcher m = PHONE_PATTERN.matcher(phoneNum);
-                if(!m.matches()){
+                if(row==null){
+                	result.put("message", "文件内容格式有误,第"+(rowNum+1)+"行为空");
+                	return result;
+                }
+                XSSFCell cell = row.getCell(0);
+                if(cell==null){
+                	result.put("message", "文件内容格式有误，第"+(rowNum+1)+"行，第1列为空");
+                	return result;
+                }
+                String number =  getValue(cell).replaceAll("\\s+", "");//去掉空格
+                Matcher m = PHONE_PATTERN.matcher(number);
+                Matcher m2 = APPID_PATTERN.matcher(number);
+                if(!m.matches()&&!m2.matches()){
                     //手机号码格式不准确
-                    String tip = "第" + (rowNum+1) + "行手机号码格式不准确，手机号：" + phoneNum + "！";
+                    String tip = "第" + (rowNum+1) + "行数据格式不准确，数据：" + number + "！";
                     errorMessage += tip;
                     LOGGER.warn(tip);
                     continue;
                 }
-                if(checkSet.contains(phoneNum)){
+                if(checkSet.contains(number)){
                     //手机号码重复
-                    String tip = "第" + (rowNum+1) + "行存在重复手机号，手机号：" + phoneNum + "，不再重复导入！";
+                    String tip = "第" + (rowNum+1) + "行存在重复数据，数据：" + number + "，不再重复导入！";
                     errorMessage += tip;
                     LOGGER.warn(tip);
                     continue;
                 }
-                checkSet.add(phoneNum);
+                checkSet.add(number);
             }
             if(CollectionUtils.isNotEmpty(checkSet)){
                 String idStr = UUIDUtils.getUUID();
