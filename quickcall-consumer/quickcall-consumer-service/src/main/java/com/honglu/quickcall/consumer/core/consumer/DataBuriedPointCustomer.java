de5b5d788c00b324fbc8ac4c098ecbd9724b070c
xiangping <xiangping@xiaoniu.com>
2018-11-17 21:52:45 +0800

    producer:转码

--- quickcall-consumer/quickcall-consumer-service/src/main/java/com/honglu/quickcall/consumer/core/consumer/DataBuriedPointCustomer.java
+++ quickcall-consumer/quickcall-consumer-service/src/main/java/com/honglu/quickcall/consumer/core/consumer/DataBuriedPointCustomer.java
@@ -101,24 +101,24 @@ public class DataBuriedPointCustomer {
 
     public void consumerMessage(Message message, Channel channel) throws Exception {
         try {
-//            LOGGER.info("接收到rabbit消息:"+message.getMessageProperties());
-//            LOGGER.info("consumer--headers:"+message.getMessageProperties().getHeaders());
-//
-//            Map<String, Object> body = message.getMessageProperties().getHeaders();
-//            Object param = body.get("messageBody");
-//
-//            LOGGER.info("consumer--body:"+param);
-//            Map<String,Object> data = JSON.parseObject((String)param,Map.class);
-//            LOGGER.info("consumer--data:"+data.toString());
-//            String type = (String)data.get("type");
-//            LOGGER.info("========开始消费=========");
+            LOGGER.info("接收到rabbit消息:"+message.getMessageProperties());
+            LOGGER.info("consumer--headers:"+message.getMessageProperties().getHeaders());
 
-            String json = new String(message.getBody(), encoding);
-            LOGGER.info("数据埋点消费端收到--【BuriedPointCustomer】 RabbitMQ消息 :" + json);
-            LOGGER.info("consumer--:"+message.getMessageProperties()+":"+ new String(message.getBody()));
-            Map<String,Object> data = JSON.parseObject(json);
+            Map<String, Object> body = message.getMessageProperties().getHeaders();
+            Object param = body.get("messageBody");
+
+            LOGGER.info("consumer--body:"+param);
+            Map<String,Object> data = JSON.parseObject((String)param,Map.class);
+            LOGGER.info("consumer--data:"+data.toString());
             String type = (String)data.get("type");
             LOGGER.info("========开始消费=========");
+
+//            String json = new String(message.getBody(), encoding);
+//            LOGGER.info("数据埋点消费端收到--【BuriedPointCustomer】 RabbitMQ消息 :" + json);
+//            LOGGER.info("consumer--:"+message.getMessageProperties()+":"+ new String(message.getBody()));
+//            Map<String,Object> data = JSON.parseObject(json);
+//            String type = (String)data.get("type");
+//            LOGGER.info("========开始消费=========");
 
             if (EventEnums.EVENT_getCode.getValue().equals(type)){
                 dataBuriedPointService.saveGetCodeData(data);
